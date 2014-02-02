9160178cbe0dab6f9bca3ffeeb25cdcb9b02eb88
Tom Booth <tombooth@gmail.com>
2014-02-02 14:48:39 +0000

    Rearrage main fn
    Do not try and retrieve any args until we actually need them.
    
    Fixes an issue with the docker path not being passed thorugh to
    worker/create-n

--- src/civil_ci/core.clj
+++ src/civil_ci/core.clj
@@ -22,34 +22,36 @@ Options:
   --config-template=<url|path>  Git repo to clone when create a config directory
                                 [default:https://github.com/tombooth/civil-ci-template.git]
   --workers=<num>               Number of worker threads. [default:2]
+  --docker-path=<path>          Path to the docker executable. [default:/usr/bin/docker]
 ")
 
 (def version "Civil CI 0.1.0")
 
 (defn -main [& args]
-  (let [arg-map (dm/match-argv (dc/parse usage-string) args)
-        path (arg-map "<config-path>")
-        port (Integer/parseInt (arg-map "--port"))
-        num-workers (Integer/parseInt (arg-map "--workers"))]
+  (let [arg-map (dm/match-argv (dc/parse usage-string) args)]
     (cond
      (or (nil? arg-map)
          (arg-map "--help")) (println usage-string)
          
      (arg-map "--version")   (println version)
          
-     :else (if-let [repo (git/get-or-create-config-repo path (arg-map "--config-template"))]
-             (if-let [server-config (data/get-server-config path repo)]
-               (let [job-config (data/get-job-config path repo server-config)
-                     job-history (data/get-job-history path server-config)
-                     build-buffer (worker/unbounded-buffer)
-                     build-channel (worker/build-channel build-buffer)]
-                 (httpkit/run-server (http/bind-routes repo server-config
-                                                       job-config job-history
-                                                       build-channel build-buffer)
-                                     {:port port})
-                 (worker/create-n num-workers build-channel job-history)
-                 (println "Started"))
-               (println "Failed to load server.json"))
-             (println "Failed to load configuration repository")))))
+     :else (let [path (arg-map "<config-path>")
+                 port (Integer/parseInt (arg-map "--port"))
+                 num-workers (Integer/parseInt (arg-map "--workers"))
+                 docker-path (arg-map "--docker-path")]
+             (if-let [repo (git/get-or-create-config-repo path (arg-map "--config-template"))]
+               (if-let [server-config (data/get-server-config path repo)]
+                 (let [job-config (data/get-job-config path repo server-config)
+                       job-history (data/get-job-history path server-config)
+                       build-buffer (worker/unbounded-buffer)
+                       build-channel (worker/build-channel build-buffer)]
+                         (httpkit/run-server (http/bind-routes repo server-config
+                                                               job-config job-history
+                                                               build-channel build-buffer)
+                                             {:port port})
+                         (worker/create-n num-workers build-channel job-history docker-path)
+                         (println "Started"))
+                 (println "Failed to load server.json"))
+               (println "Failed to load configuration repository"))))))
 
 
